8a89feaebfc6c66f9d849c1078ec3cc8027a89a6
pandafulmanda <pandafulmanda@gmail.com>
2015-09-13 00:53:59 -0500

    fix select for editable params to be on focus, run code on enter keypress

--- assets/help/help.js
+++ assets/help/help.js
@@ -4,17 +4,44 @@ function setupHelp(){
 
   _.each(justEdits, function(editElement){
     editElement.onclick = function(clickEvent){
-      document.execCommand('selectAll',false,null);
       clickEvent.stopPropagation();
+    }
+    editElement.onfocus = function(focusEvent){
+      var focusedElement = this;
+      requestAnimationFrame(function() {
+        selectElementContents(focusedElement);
+      });
+    }
+
+    editElement.onkeydown = function(keyEvent){
+      if(keyEvent.keyCode === 13){
+        window.editElement = editElement;
+        keyEvent.preventDefault();
+        runCode(this.closest('code'));
+      }
     }
   });
 
   _.each(canRuns, function(codeElement){
     codeElement.onclick = function(clickEvent){
       clickEvent.preventDefault();
-      var code = this.innerText;
-      code = code.replace('var ', 'window.')
-      console.log(eval(code));
+      runCode(this);
     }
   });
+
+
+  function runCode(element){
+    var code = element.innerText;
+    code = code.replace('var ', 'window.')
+    console.log(eval(code));
+  }
+
+  function selectElementContents(element) {
+    var range = document.createRange();
+    range.selectNodeContents(element);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 }
+
